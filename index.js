974e36e0138de02622ec3de72698918fe2be260e
Bernard <bernard.koech@student.moringaschool.com>
2022-08-20 20:55:19 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = 'Bernard Koech';
+
+const welcomeMessage = 'Welcome to Flatbook, Bernard Koech!';
+
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, BERNARD KOECH!';
+
+const shortGreeting = 'Welcome, Bernard!';
+
+
+
